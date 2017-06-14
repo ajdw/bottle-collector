1b9be1e2dc6266801805df0251797bd169979845
Alex Williams <alexanderjdwilliams@gmail.com>
2017-06-14 16:01:03 -0700

    added user input function

--- js/main.js
+++ js/main.js
@@ -1,23 +1,43 @@
-  $( function() {
-    var availableTags = [
-      'Aquafina',
-      'Arrowhead Water',
-      'Crystal Geyser',
-      'Dasani',
-      'Dejà Blue',
-      'Evian',
-      'Fiji Water',
-      'Glaceau',
-      'Mountain Valley Spring Water',
-      'Nestlé Pure Life',
-      'Panama Blue',
-      'Perrier',
-      'San Pellegrino',
-      'Sierra Springs',
-      'Smart Water',
-      'VOSS'
-    ];
-    $( '#tags' ).autocomplete({
-      source: availableTags
-    });
-  } );
+// Our various types of water bottle brands stored in an array
+var waterBottles = [
+      'Aquafina','Arrowhead Water', 'Crystal Geyser', 'Dasani', 'Dejà Blue', 'Evian', 'Fiji Water', 'Glaceau',
+      'Mountain Valley Spring Water', 'Nestlé Pure Life', 'Panama Blue', 'Perrier', 'San Pellegrino', 'Sierra Springs',
+      'Smart Water', 'VOSS'
+      ]
+// Auto complete method
+fuzzyAutocomplete($('#bottleInput'), waterBottles);
+
+// User generated bottle values will go in this array
+var userBottles = [];
+
+
+// Grab the users input of water bottle name
+function getBottles(){
+      console.log('working');
+      var userInput = document.getElementById('bottleInput').value;
+      userBottles.push(userInput);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
