0668f440817adc1d7c762f0f926bb6a6137676ef
Alex Williams <alexanderjdwilliams@gmail.com>
2017-07-11 19:56:36 -0700

    added new can

--- js/main.js
+++ js/main.js
@@ -34,18 +34,19 @@ var zolaBottle = new Bottle('Zola', 'img/zola.png');
 var squirtCan = new Bottle('Squirt', 'img/squirt.png');
 var hawaiianPunchCan = new Bottle('Hawaiian Punch', 'img/hawaiianpunch.png');
 var surgeCan = new Bottle('Surge', 'img/surge.png');
+var coorsCan = new Bottle('Coors', 'img/coors.png');
 
 
 // Pushing our bottles to our bottleObjectArray
 bottleObjectArray.push(aquafinaBottle, pepsiCan, vossBottle, evianBottle, dasaniBottle, arrowheadBottle, fijiBottle, cokeCan, 
   crystalGeyserBottle, mountainDewCan, fantaCan, qureBottle, drPepperCan, smartwaterBottle, budweiserBottle, heinekenBottle, schweppesCan, lacroixCan,
-  sevenUpCan, spriteCan, zolaBottle, squirtCan, hawaiianPunchCan, surgeCan); 
+  sevenUpCan, spriteCan, zolaBottle, squirtCan, hawaiianPunchCan, surgeCan, coorsCan); 
 
 // Push our bottle object names to our bottles array so that way in can be autofilled when typing in our index field 
 bottles.push(aquafinaBottle.name, pepsiCan.name, vossBottle.name, evianBottle.name, dasaniBottle.name, arrowheadBottle.name,
 fijiBottle.name, cokeCan.name, crystalGeyserBottle.name, qureBottle.name, smartwaterBottle.name, mountainDewCan.name, fantaCan.name, drPepperCan.name,
 budweiserBottle.name, heinekenBottle.name, schweppesCan.name, lacroixCan.name, sevenUpCan.name, spriteCan.name, zolaBottle.name,
-squirtCan.name, hawaiianPunchCan.name, surgeCan.name); 
+squirtCan.name, hawaiianPunchCan.name, surgeCan.name, coorsCan.name); 
 
 // User generated bottle values will go in this array
 var userBottles = [];
