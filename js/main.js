484b6668a284bee1910f28683329226c2198a476
Alex Williams <alexanderjdwilliams@gmail.com>
2017-08-10 18:27:10 -0700

    added Zima

--- js/main.js
+++ js/main.js
@@ -43,6 +43,7 @@ var aAndWCan = new Bottle('A&W Root Beer', 'img/a&w.png');
 var sapporoCan = new Bottle('Sapporo', 'img/sapporo.png');
 var mrPibbCan = new Bottle('Mr. Pibb', 'img/mrpibb.png');
 var crystalPepsiBottle = new Bottle('Crystal Pepsi', 'img/crystalpepsi.png');
+var zimaBottle = new Bottle('Zima', 'img/zima.png');
 
 // Generics
 var colaGenericCan = new Bottle('Cola (generic)', 'img/cola.png');
@@ -52,14 +53,15 @@ var waterGenericBottle = new Bottle('Bottle (generic)', 'img/bottle-generic.png'
 bottleObjectArray.push(aquafinaBottle, pepsiCan, vossBottle, evianBottle, dasaniBottle, arrowheadBottle, fijiBottle, cokeCan, 
 crystalGeyserBottle, mountainDewCan, fantaCan, qureBottle, drPepperCan, smartwaterBottle, budweiserBottle, heinekenBottle, schweppesCan, lacroixCan,
 sevenUpCan, spriteCan, zolaBottle, squirtCan, hawaiianPunchCan, surgeCan, coorsCan, fourLokoCanLemonade, budLightCan, colaGenericCan, 
-gatoradeBottle, poweradeBottle, aAndWCan, sapporoCan, mrPibbCan, crystalPepsiBottle, waterGenericBottle); 
+gatoradeBottle, poweradeBottle, aAndWCan, sapporoCan, mrPibbCan, crystalPepsiBottle, waterGenericBottle, zimaBottle); 
 
 // Push our bottle object names to our bottles array so that way in can be autofilled when typing in our index field 
 bottles.push(aquafinaBottle.name, pepsiCan.name, vossBottle.name, evianBottle.name, dasaniBottle.name, arrowheadBottle.name,
 fijiBottle.name, cokeCan.name, crystalGeyserBottle.name, qureBottle.name, smartwaterBottle.name, mountainDewCan.name, fantaCan.name, drPepperCan.name,
 budweiserBottle.name, heinekenBottle.name, schweppesCan.name, lacroixCan.name, sevenUpCan.name, spriteCan.name, zolaBottle.name,
 squirtCan.name, hawaiianPunchCan.name, surgeCan.name, coorsCan.name, fourLokoCanLemonade.name, colaGenericCan.name, budLightCan.name,
-gatoradeBottle.name, poweradeBottle.name, aAndWCan.name, sapporoCan.name, mrPibbCan.name, crystalPepsiBottle.name, waterGenericBottle.name); 
+gatoradeBottle.name, poweradeBottle.name, aAndWCan.name, sapporoCan.name, mrPibbCan.name, crystalPepsiBottle.name, waterGenericBottle.name,
+zimaBottle.name); 
 
 // User generated bottle values will go in this array
 var userBottles = []; 
@@ -189,7 +191,7 @@ function submitBottles(){
   }
 
   console.log(data);
-  var ref = database.ref('User Bottle Collection');
+  var ref = database.ref('User Data');
   alert('Thank you for submitting');
   ref.push(data);
 }
